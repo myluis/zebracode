0401f20dd10933a7d02079a7cb6f1df595049f14
myluis <ibhluis@gmail.com>
2020-12-13 18:25:20 +0000

    The category class added and some changes made to the HHPSD file

--- src/Category.java
+++ src/Category.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class HandHeldProductScannerDevice {
+public class Category {
 	
 	JFrame frame;
 	JPanel panel;
@@ -12,13 +12,14 @@ public class HandHeldProductScannerDevice {
 	JTextField pwd_txt;
 //	JButton btn_sbmt;
 	JButton btn_ok;
+	JButton btn_nums;
 	JButton btn_cancel;
 	
-	public HandHeldProductScannerDevice() {
-		HandHeldProductScannerDevice();
+	public Category() {
+		Category();
 	}
 	
-	public void HandHeldProductScannerDevice() {
+	public void Category() {
 		
 		frame = new JFrame();
 		panel = new JPanel();
@@ -27,7 +28,6 @@ public class HandHeldProductScannerDevice {
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-//		frame.setLayout(new FlowLayout());
 //		frame.setResizable(false);
 		frame.add(panel);
 		
@@ -35,7 +35,7 @@ public class HandHeldProductScannerDevice {
 		                      //It makes components align or display correctly.
 		
 		//Here we add the app title
-		app_title = new JLabel("Login");
+		app_title = new JLabel("Product Location");
 		app_title.setSize(120, 35);
 		app_title.setBounds(170, 10, 120, 35);
 		panel.add(app_title);
@@ -46,14 +46,20 @@ public class HandHeldProductScannerDevice {
 		btn_ok.setBounds(100, 45, 60, 35);
 		panel.add(btn_ok);
 		
+		//Here we add a new button to it for number, nums
+		btn_nums = new JButton("Num");
+		btn_nums.setSize(55, 35);
+		btn_nums.setBounds(170, 45, 60, 35);
+		panel.add(btn_nums);
+		
 		//Here we add the cancel button
 		btn_cancel = new JButton("ESC");
 		btn_cancel.setSize(55, 35);
-		btn_cancel.setBounds(170, 45, 60, 35);
+		btn_cancel.setBounds(240, 45, 60, 35);
 		panel.add(btn_cancel);
 		
 		//Here we add the username and username textfield to it
-		lbl_name = new JLabel("Username");
+		lbl_name = new JLabel("Bar Code");
 		lbl_name.setSize(130, 40);
 		lbl_name.setBounds(30, 90, 90, 40);
 		panel.add(lbl_name);
@@ -63,12 +69,12 @@ public class HandHeldProductScannerDevice {
 //		name_txt.setBorder(null);
 		name_txt.setSize(180, 40);
 		name_txt.setBounds(100, 90, 200, 40);
-		name_txt.setBackground(Color.green);
+		name_txt.setBackground(Color.orange);
 		panel.add(name_txt);
 		
 		
 		//The label for the password
-		lbl_pass = new JLabel("Password");
+		lbl_pass = new JLabel("Shelf Area");
 		lbl_pass.setSize(130, 40);
 		lbl_pass.setBounds(30, 140, 90, 40);
 		panel.add(lbl_pass);
@@ -84,16 +90,22 @@ public class HandHeldProductScannerDevice {
 		btn_ok.setBounds(100, 190, 60, 35);
 		panel.add(btn_ok);
 		
+		//Here we add a new button to it for number, nums
+		btn_nums = new JButton("Num");
+		btn_nums.setSize(55, 35);
+		btn_nums.setBounds(170, 190, 60, 35);
+		panel.add(btn_nums);
+		
 		//Here we add the cancel button
 		btn_cancel = new JButton("ESC");
 		btn_cancel.setSize(55, 35);
-		btn_cancel.setBounds(170, 190, 60, 35);
+		btn_cancel.setBounds(240, 190, 60, 35);
 		panel.add(btn_cancel);
 
 	}
 	
 	public static void main(String[] args) {
-		new HandHeldProductScannerDevice();
+		new Category();
 	}
 	
 
